3655b844a6beba8468f65a31515fb37f4dd32cf9
Ming Cheng <cmgrace@bu.edu>
2021-04-27 12:09:51 -0400

    revise

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-//export const API_ENDPOINT = "https://shrouded-forest-99630.herokuapp.com/api";
-export const API_ENDPOINT = "http://localhost:8000/api";
+export const API_ENDPOINT = "https://shrouded-forest-99630.herokuapp.com/api";
+//export const API_ENDPOINT = "http://localhost:8000/api";
 export const TOKEN_KEY = "amazon-client-auth-token";
